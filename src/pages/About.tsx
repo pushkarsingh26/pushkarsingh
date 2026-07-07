4e1d31ced9c4b2149178273d780783139b4f9f60
pushkarsingh26 <pushkarcd123@gmail.com>
2026-07-07 16:30:08 +0530

    Changed the portfolio link

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -824,7 +824,7 @@ export default function About() {
                         <div class="contact-item">
                             <!-- Globe SVG -->
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>
-                            <a href="https://pushkarsingh26.github.io/pushkarsingh/" target="_blank">Portfolio Website</a>
+                            <a href="https://pushkarsingh-26.vercel.app/" target="_blank">Portfolio Website</a>
                         </div>
                     </div>
                 </div>
